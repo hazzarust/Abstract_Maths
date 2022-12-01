8de295968694c939889fa3b8772d5a23094e74f4
Harry <harry.grant125@gmail.com>
2022-12-01 14:39:58 +0000

    Added some comments

--- basic_generator/src/main.rs
+++ basic_generator/src/main.rs
@@ -4,7 +4,16 @@
 pub enum Error{
     NoGenerator,
 }
+
 //Make a function that will go through the vector and find out what the generator numbers are
+//Takes a vector of size u8, makes a copy
+//iterates through the vec we passed into function, the element of current loop is stored under x
+//I will then iterate again for each element x of our inital first loop
+//This time I will perform computation using the enumeration of the current loop and the element x from our first loop
+//(current enumeration of this loop) * x (from our initial loop) % 12 for every enumeration element in our 2nd loop
+//The result is then stored under computated vec.
+//I then sort it, and compare to the original copy vec, to see if x element generated the inital vector using the computation above
+//If they match, then I will push x into a vector and return all possible generators after x has finished looping through the whole vector
 
 pub fn generator(vec: &Vec<u8>) -> Result<Vec<u8>, Error>{
 
